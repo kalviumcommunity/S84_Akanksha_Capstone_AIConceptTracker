8234e0f019240fb659928a19cc561fbfbf15ff42
akankshahu <akanksha.kumari.s84@kalvium.community>
2025-04-22 11:11:13 +0530

    Database created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const mongoose=require('mongoose');
-const {User,Concept}=require('./models/schema');
+const User=require('./models/userSchema');
+const Concept=require('./models/conceptSchema');
+
 const cors=require('cors');
 require('dotenv').config();
 const app=express();
@@ -8,7 +10,7 @@ const port=process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log("ConnEcetd to database")) 
+    .then(()=>console.log("Connected to database")) 
     .catch((error)=>console.error("Failed to connect to database:",error));
 
 app.get("/",(req,res)=>res.send({message:"Server is running"}));
